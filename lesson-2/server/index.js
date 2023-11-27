c73e8b18e6838245f9911b2869153e377c3dad8e
sangcia113 <sangpham0972868740@gmail.com>
2023-11-27 21:00:10 +0700

    first code

---
+++ lesson-2/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
